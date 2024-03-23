6cf95bfbad99317b11ceae954b27a1b11f8c9e2c
davidpc99 <71788370+davidpc99@users.noreply.github.com>
2024-03-23 17:46:20 +0100

    Get data script

---
+++ get_data.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+import pandas as pd
+import os
+
+def save_as_csv(dataset, filename):
+    df = pd.DataFrame(dataset)
+    df.to_csv(filename, index=False)
+
+
+def main():
+    dir = "data"
+    # Si el fichero no existe guardamos el dataset
+    
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+        dataset = load_dataset("wikimedia/wikipedia", "20231101.en", split="train[:1%]")
+        dataset.save_to_disk("data/wiki_corpus")
+        dataset = load_dataset("wiki_qa")
+        dataset.save_to_disk("data/qa_corpus")
+
+if __name__ == "__main__":
+    main()
