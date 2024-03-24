24030ce775db404ac838dbb9154a189abb22ed9e
David Pastor Crespo <davidgasol913@gmail.com>
2024-03-24 16:37:59 +0100

    Get data

--- get_data.py
+++ get_data.py
@@ -13,7 +13,8 @@ def main():
     
     if not os.path.isdir(dir):
         os.mkdir(dir)
-        dataset = load_dataset("wikimedia/wikipedia", "20231101.en", split="train[:1%]")
+        #dataset = load_dataset("wikimedia/wikipedia", "20231101.en", split="train[:1%]+train[20%:21%]+train[40%:41%]+train[60%:61%]+train[80%:81%]")
+        dataset = load_dataset("wikimedia/wikipedia", "20231101.en", split="train")
         dataset.save_to_disk("data/wiki_corpus")
         dataset = load_dataset("wiki_qa")
         dataset.save_to_disk("data/qa_corpus")
